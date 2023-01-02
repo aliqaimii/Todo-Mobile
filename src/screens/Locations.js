7eac2fcfc13b68e6f879cfb4615569e2fcee1ed7
Ali Raza <aliraza@webhr.email>
2023-01-02 10:59:51 +0500

    remove inline style in locations.js

--- src/screens/Locations.js
+++ src/screens/Locations.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Image, FlatList } from "react-native";
+import { View, Text, Image, FlatList, StyleSheet } from "react-native";
 import { GetCheckins } from "../services/Api";
 
 const Locations = () => {
@@ -17,29 +17,25 @@ const Locations = () => {
   };
 
   const preLoca = ({ item }) => (
-    <View style={{ margin: 5, flexDirection: "row", padding: 7 }}>
+    <View style={styles.item}>
       <Image
-        style={{ width: 20, height: 20 }}
+        style={styles.itemImg}
         source={require("../assets/locationPin.png")}
       />
-      <View style={{ marginLeft: 8 }}>
-        <Text style={{ fontSize: 17, marginBottom: 5 }}> {item?.address}</Text>
+      <View style={styles.itemInner}>
+        <Text style={styles.addressText}> {item?.address}</Text>
 
-        <Text style={{ fontSize: 14, color: "gray" }}>
+        <Text style={styles.latLongText}>
           {item?.latitude + ", " + item?.latitude}
         </Text>
       </View>
     </View>
   );
   return (
-    <View style={{ height: "100%", padding: 20, backgroundColor: "white" }}>
-      <Text style={{ fontWeight: "bold", marginVertical: 10, fontSize: 18 }}>
-        Current Location
-      </Text>
+    <View style={styles.main}>
+      <Text style={styles.currLocText}>Current Location</Text>
 
-      <Text style={{ fontWeight: "bold", marginVertical: 10, fontSize: 18 }}>
-        Previous Locations
-      </Text>
+      <Text style={styles.prevLocText}>Previous Locations</Text>
       <FlatList
         data={prevLocations}
         renderItem={preLoca}
@@ -48,5 +44,43 @@ const Locations = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  main: {
+    height: "100%",
+    padding: 20,
+    backgroundColor: "white",
+  },
+  currLocText: {
+    fontWeight: "bold",
+    marginVertical: 10,
+    fontSize: 18,
+  },
+  prevLocText: {
+    fontWeight: "bold",
+    marginVertical: 10,
+    fontSize: 18,
+  },
+  item: {
+    margin: 5,
+    flexDirection: "row",
+    padding: 7,
+  },
+  itemImg: {
+    width: 20,
+    height: 20,
+  },
+  itemInner: {
+    marginLeft: 8,
+  },
+  addressText: {
+    fontSize: 17,
+    marginBottom: 5,
+  },
+  latLongText: {
+    fontSize: 14,
+    color: "gray",
+  },
+});
 
 export default Locations;
